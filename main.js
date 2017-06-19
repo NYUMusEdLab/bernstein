628762b5dde7d0e0f4b65859b4b9d1fd625b94bf
Casey Voss <caseyvos@umich.edu>
2017-06-19 11:47:42 -0400

    Clean up JS for demo

--- main.js
+++ main.js
@@ -2,11 +2,14 @@ function main() {
 
 
 	$("#searchbutton").click(function(){
-		alert("spicy");
 
-		var new_search = $("#video1");
-		new_search.prop("data-search", "tuning");
-		location.reload();
+		//var new_search = $("#video1");
+		var search_value = $("#search-bar").val();
+		alert(search_value);
+
+		d = new Date();
+		$("#video1").attr("data-search", search_value+d.getTime());
+
 
 
 
